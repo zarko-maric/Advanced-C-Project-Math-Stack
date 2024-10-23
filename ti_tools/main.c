083543b4c920ec9dc08bd70de053c0318934305c
zarko-maric <110539951+zarko-maric@users.noreply.github.com>
2024-10-23 12:26:35 +0200

    Update main.c

--- ti_tools/main.c
+++ ti_tools/main.c
@@ -30,22 +30,18 @@ int32_t main(void) {
     check_parentheses(input);
     printf("\n");
 
-    /* Test 1: Ispravno uparene zagrade */
     const char* test1 = "(1 + 2) * (3 + (4 - 5))";
     printf("Test 1: Provera zagrada za izraz: %s\n", test1);
     check_parentheses(test1);
 
-    /* Test 2: Nedostaje zatvorena zagrada */
     const char* test2 = "((1 + 2) * (3 + (4 - 5)";
     printf("\nTest 2: Provera zagrada za izraz: %s\n", test2);
     check_parentheses(test2);
 
-    /* Test 3: Višak zatvorenih zagrada */
     const char* test3 = "(1 + 2)) * (3 + (4 - 5))";
     printf("\nTest 3: Provera zagrada za izraz: %s\n", test3);
     check_parentheses(test3);
 
-    /* Test 4: Nema zagrada */
     const char* test4 = "1 + 2 * 3 + 4 - 5";
     printf("\nTest 4: Provera zagrada za izraz: %s\n", test4);
     check_parentheses(test4);
